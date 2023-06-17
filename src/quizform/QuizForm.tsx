e1de7cc2551b211c47030216b0b62ed02c0b3755
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-06-17 14:15:28 -0700

    Implemented initial QuizForm component and styles file

--- src/quizform/QuizForm.tsx
+++ src/quizform/QuizForm.tsx
@@ -1,10 +1,35 @@
-// import React from 'react';
+import './QuizForm.css';
 
-const QuizForm = () => {
+function QuizForm() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <form className='quizform'>
+      <div className='name-input-container'>
+        <input
+          required
+          type='text'
+          id='quiz-name-input'
+          placeholder='What is the name of your quiz?'
+        />
+      </div>
+
+      <div className='question-list'>
+        <h2>elements to go here</h2>
+
+      </div>
+
+      <div className='form-controls'>
+        <button
+          type='button'
+        >
+          Submit Quiz
+        </button>
+        <button
+          type='button'
+        >
+          + New Question
+        </button>
+      </div>
+    </form>
   );
 }
 
