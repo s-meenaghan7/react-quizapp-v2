975eb353b65df520b2c5e675c6395f3ca0b354dd
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-06-17 12:51:52 -0700

    Created the Quizform package and tsx file. Cleaned up some other files and established some base CSS in main.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './main.css'
+import QuizForm from './quizform/QuizForm'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QuizForm />
   </React.StrictMode>,
 )
