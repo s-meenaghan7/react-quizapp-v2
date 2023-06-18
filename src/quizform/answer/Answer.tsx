60800b83235b17c685fcc43737e99ebdaac32006
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-06-18 13:35:49 -0700

    Implemented Answer component/styling for the QuestionModal

---
+++ src/quizform/answer/Answer.tsx
@@ -0,0 +1,42 @@
+import './Answer.css';
+
+type Answer = {
+  id: number;
+  answer: string;
+  isCorrect: boolean;
+}
+
+const Answer: React.FC<Answer> = ({ id, answer, isCorrect }) => {
+  return (
+    <tr className='answer'>
+      <td id='answer-id'>
+        {id}
+      </td>
+
+      <td>
+        <input 
+          type='text'
+          name='answer'
+          className='answer-input'
+          id={`answer${id}`}
+          placeholder={`Answer ${id}`}
+          defaultValue={answer}
+          required
+        />
+      </td>
+
+      <td>
+        <input 
+          type='radio'
+          name='isCorrect'
+          className='radio'
+          id={`radio${id}`}
+          defaultChecked={isCorrect}
+        />
+        <label htmlFor={`radio${id}`}></label>
+      </td>
+    </tr>
+  );
+}
+
+export default Answer;
