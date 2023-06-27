338762c7f53c91411a70067e9213b8cbf1fea812
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-06-26 18:33:43 -0700

    Implemented confirmation for deleting of questions

--- src/quizform/question/QuestionComponent.tsx
+++ src/quizform/question/QuestionComponent.tsx
@@ -13,44 +13,82 @@ type QuestionComponentProps = {
 
 const QuestionComponent: React.FC<QuestionComponentProps> = ({ id, question, options, questionsDispatch }) => {
   const [questionModalOpen, setQuestionModalOpen] = useState(false);
+  const [isPendingDelete, setIsPendingDelete] = useState(false);
+
+  function deleteQuestion() {
+    setIsPendingDelete(false);
+    questionsDispatch({ type: 'DELETE_QUESTION', id: id });
+  }
 
   return (
     <>
-      <div className="question-component centered">
+      <div className={`question-component centered ${isPendingDelete ? 'pending-delete' : ''}`}>
         <div className='question-id'>
           {id}.
         </div>
+        {
+          isPendingDelete ?
+            <>
+              <div className='confirmation'>
+                Delete question {id} (Answers: {options.length})?
+              </div>
+              <div className='question-controls'>
+                <button
+                  type='button'
+                  className='question-btn'
+                  title='Confirm Delete Question'
+                  onClick={deleteQuestion}
+                >
+                  <span id='confirm' className="material-icons-round">
+                    check
+                  </span>
+                </button>
+                <button
+                  type='button'
+                  className='question-btn'
+                  title='Cancel Delete Question'
+                  onClick={() => setIsPendingDelete(false)}
+                >
+                  <span id='' className="material-icons-round">
+                    close
+                  </span>
+                </button>
+              </div>
+            </>
+            :
+            <>
+              <div className='question-section'>
+                {question}
+              </div>
 
-        <div className='question-section'>
-          {question}
-        </div>
+              <div className='question-controls'>
+                <button
+                  type='button'
+                  className='question-btn'
+                  title={`Edit question ${id}`}
+                  onClick={() => setQuestionModalOpen(true)}
+                >
+                  <span id='edit' className="material-icons-round">
+                    edit
+                  </span>
+                </button>
+                <button
+                  type='button'
+                  className='question-btn'
+                  title={`Delete question ${id}`}
+                  onClick={() => setIsPendingDelete(true)}
+                >
+                  <span id='delete' className="material-icons-round">
+                    delete_forever
+                  </span>
+                </button>
+              </div>
 
-        <div className='question-controls'>
-          <button
-            type='button'
-            className='question-btn'
-            title='Edit question'
-            onClick={() => setQuestionModalOpen(true)}
-          >
-            <span id='edit' className="material-icons-round">
-              edit
-            </span>
-          </button>
-          <button
-            type='button'
-            className='question-btn'
-            title='Delete question'
-            onClick={() => questionsDispatch({ type: 'DELETE_QUESTION', id: id })}
-          >
-            <span id='delete' className="material-icons-round">
-              delete_forever
-            </span>
-          </button>
-        </div>
-
-        <div className='answers-length'>
-          ({options.length} answers)
-        </div>
+              <div className='answers-length'>
+                (Answers: {options.length})
+              </div>
+            </>
+        }
       </div>
 
       <QuestionModal
