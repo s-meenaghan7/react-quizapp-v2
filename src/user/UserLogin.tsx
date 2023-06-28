bf8bf2e2f8479f98ed1d64fe29f912e007d71b93
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-06-28 14:18:52 -0700

    Created user package and a UserLogin component to create a form for users to login with.

---
+++ src/user/UserLogin.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type UserLoginProps = {}
+
+const UserLogin: React.FC<UserLoginProps> = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default UserLogin;
