5d53e647475ad99fa8cdfb4f95366132a93ac656
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-06-28 12:34:24 -0700

    Installed react-router-dom to project

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './main.css'
 import QuizFormContainer from './quizform/QuizFormContainer'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <QuizFormContainer />
+    <BrowserRouter>
+
+      <QuizFormContainer />
+      
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
