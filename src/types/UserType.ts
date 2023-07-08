dbca36e71b635997e4a5eae77370894d805abe94
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-07-08 12:16:56 -0700

    Updated QuizMeUser type to support an array of Quiz data

--- src/types/UserType.ts
+++ src/types/UserType.ts
@@ -1,6 +1,9 @@
+import { Quiz } from "./QuizTypes";
+
 export type QuizMeUser = {
   id?: any | null;
   fullName: string;
   email: string;
   password: string;
+  quizzes: Quiz[]; // confirm with backend and database
 }
