87f700f28fbfea48a402850ed1f58cf55b358efd
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-07-07 18:31:03 -0700

    Implemented resendVerificationEmail method in auth.service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-const API_URL = "https://localhost:8080/api/v1/";
+const API_URL = "http://localhost:8080/api/v1/";
 
 export const registerNewUser = (name: string, email: string, password: string) => {
   return axios.post(API_URL + "register", {
     name, email, password
   });
 };
+
+export const resendVerificationEmail = (email: string) => {
+  return axios.post(API_URL + "register/resend-email", {
+    email
+  });
+}
 
 export const login = (username: string, password: string) => {
   return axios.post(API_URL + "login", {
